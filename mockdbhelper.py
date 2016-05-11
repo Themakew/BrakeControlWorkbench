f3e88bdbee7ad505be3f87ee0c9aaa0eb3895058
Ruyther Costa <ruy_fusion@hotmail.com>
2016-05-11 16:35:28 -0300

    Adding a secret key for the app

---
+++ mockdbhelper.py
@@ -0,0 +1,9 @@
+MOCK_USERS = {'admin': 'admin'}
+
+
+class MockDBHelper:
+
+    def get_user(self, email):
+        if email in MOCK_USERS:
+            return MOCK_USERS[email]
+        return None
--- waitercaller.py
+++ waitercaller.py
@@ -2,8 +2,18 @@ from flask import Flask
 from flask import render_template
 from flask.ext.login import LoginManager
 from flask.ext.login import login_required
+from flask.ext.login import login_user
+from mockdbhelper import MockDBHelper as DBHelper
+from user import User
+from flask import redirect
+from flask import url_for
+
+DB = DBHelper()
+
 
 app = Flask(__name__)
+app.secret_key = "YqO29YjPwEWcV7w5UjzoamGL7+9UazD5MWcfM6ZgN/2lvQJtcjZHH2p+wSfZt\
+7oNlW+7WQn80rvvS9C1CUWffFIHXz04qKSLkD9o"
 login_manager = LoginManager(app)
 
 
