a73601ad9ed3f8dcacd10526d6bae93cac7530f6
Ruyther Costa <ruy_fusion@hotmail.com>
2016-05-30 13:31:15 -0300

    Models tests

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 from application import app
-import models
 import views
 
 
--- app/tests/modelsTests.py
+++ app/tests/modelsTests.py
@@ -1,9 +1,32 @@
 import unittest
+import os
+import sys
+sys.path.insert(0, os.path.abspath(__file__ + "/../.."))
+from models import User
 
 
-class TestCase(unittest.TestCase):
-    def test(self):
-        pass
+class TestModels(unittest.TestCase):
+
+    def test_get_id_when_email_is_informed(self):
+        self.user = User("test@test.com")
+        self.assertEqual(self.user.get_id(), "test@test.com")
+
+    def test_get_id_when_email_is_not_informed(self):
+        self.user = User("")
+        self.assertEqual(self.user.get_id(), "")
+
+    def test_is_active(self):
+        self.user = User("test@test.com")
+        self.assertTrue(self.user.is_active())
+
+    def test_is_anonymous(self):
+        self.user = User("test@test.com")
+        self.assertFalse(self.user.is_anonymous())
+
+    def test_is_authenticated(self):
+        self.user = User("test@test.com")
+        self.assertTrue(self.user.is_authenticated())
+
 
 if __name__ == '__main__':
     unittest.main()
