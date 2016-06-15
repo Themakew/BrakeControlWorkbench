b4b097768af89e8f2718d6307e1d96213271ac31
Davi Gusmao <davi.b.gusmao@gmail.com>
2016-06-15 11:51:58 -0300

    Raspberry pi commands

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from application import app
 from application import celery
+from application import bcontrol
 import views
 
 
 if __name__ == '__main__':
+    bcontrol.turn_off_all()
     app.run('0.0.0.0', port=5000, debug=True)
